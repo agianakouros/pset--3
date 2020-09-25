0dec4cd4c04ed2dcef4e70fcfeb5cf9836c6d9fe
agianakouros <55397838+agianakouros@users.noreply.github.com>
2020-09-25 13:07:21 -0400

    committ

--- Exercise3.java
+++ Exercise3.java
@@ -5,20 +5,20 @@ public class Exercise3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Height:");
+        System.out.println("Height :");
         double height = in.nextDouble();
 
-        System.out.println("Width:");
+        System.out.println("Width :");
         double width = in.nextDouble();
 
-        double answer = width * height;
-        double finished = answer * (25.4 * 25.4);
+        double areainches = width * height;
+        double areamm = areainches * (25.4 * 25.4);
 
         DecimalFormat commaFormat;
         commaFormat = new DecimalFormat("#,###.##");
 
         System.out.println(" ");
-        System.out.println(commaFormat.format(finished) + " square millimeters.");
+        System.out.println(commaFormat.format(areamm) + " square millimeters.");
 
         in.close();
     }
