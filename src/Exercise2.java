debca5355de728970d81f318deb16cd2e6fbfcb3
agianakouros <55397838+agianakouros@users.noreply.github.com>
2020-09-27 22:31:30 -0400

    final commit

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,5 +1,3 @@
-package src;
-
 public class Exercise2{
     public static void main(String[] args) {
         System.out.println("\"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
